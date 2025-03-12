b009e7650caee35019ef80a95ceb093829518b4c
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-03-12 09:45:39 +0530

    add suspense

--- src/app/details-page/page.tsx
+++ src/app/details-page/page.tsx
@@ -1,9 +1,12 @@
 import Dashboard from "@/components/Dashboard";
+import { Suspense } from "react";
 
 export default function DetailsPage() {
     return (
       <>
+      <Suspense>
     <Dashboard/>
+    </Suspense>
       </>
     );
   }
